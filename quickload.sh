9a5d506ec1fa371d994883c63f669f43d4cde5f1
Sam C <samrocksc@gmail.com>
2016-07-21 16:04:02 -0400

    pushing up a base config file again

--- quickload.sh
+++ quickload.sh
@@ -1,29 +1,29 @@
 #Move the config files
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 npm i -g eslint estraverse estraverse-fb eslint-plugin-react babel-eslint &
-cp -R .oh-my-zsh ~/ &
-cp .eslintrc .vimrc .zshrc .ctags ~ &
+cp .eslintrc .vimrc .zshrc .ctags .tmux.conf ~ &
 mkdir -p ~/.vim/autoload ~/.vim/bundle &
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim &
 cd ~/.vim/bundle &
-git clone https://github.com/scrooloose/nerdtree &
-git clone https://github.com/jiangmiao/auto-pairs &
-git clone https://github.com/kien/ctrlp.vim &
-git clone https://github.com/eslint/eslint &
-git clone https://github.com/tomasr/molokai &
-git clone https://github.com/scrooloose/nerdcommenter &
-git clone https://github.com/ervandew/supertab &
-git clone https://github.com/scrooloose/syntastic &
-git clone https://github.com/tomtom/tlib_vim &
-git clone https://github.com/marcweber/vim-addon-mw-utils &
-git clone https://github.com/bling/vim-airline &
-git clone https://github.com/craigemery/vim-autotag &
-git clone https://github.com/tpope/vim-fugitive &
-git clone https://github.com/airblade/vim-gitgutter &
-git clone https://github.com/pangloss/vim-javascript &
-git clone https://github.com/maksimr/vim-jsbeautify &
-git clone https://github.com/tpope/vim-markdown &
-git clone https://github.com/tpope/vim-sensible &
-git clone https://github.com/garbas/vim-snipmate &
-git clone https://github.com/honza/vim-snippets &
-git clone https://github.com/bronson/vim-trailing-whitespace &
-git clone https://github.com/mxw/vim-jsx
+git clone https://github.com/scrooloose/nerdtree ~/.vim/bundle/ &
+git clone https://github.com/jiangmiao/auto-pairs ~/.vim/bundle/ &
+git clone https://github.com/kien/ctrlp.vim ~/.vim/bundle/ &
+git clone https://github.com/eslint/eslint ~/.vim/bundle/ &
+git clone https://github.com/tomasr/molokai ~/.vim/bundle/ &
+git clone https://github.com/scrooloose/nerdcommenter ~/.vim/bundle/ &
+git clone https://github.com/ervandew/supertab ~/.vim/bundle/ &
+git clone https://github.com/scrooloose/syntastic ~/.vim/bundle/ &
+git clone https://github.com/tomtom/tlib_vim ~/.vim/bundle/ &
+git clone https://github.com/marcweber/vim-addon-mw-utils ~/.vim/bundle/ &
+git clone https://github.com/bling/vim-airline ~/.vim/bundle/ &
+git clone https://github.com/craigemery/vim-autotag ~/.vim/bundle/ &
+git clone https://github.com/tpope/vim-fugitive ~/.vim/bundle/ &
+git clone https://github.com/airblade/vim-gitgutter ~/.vim/bundle/ &
+git clone https://github.com/pangloss/vim-javascript ~/.vim/bundle/ &
+git clone https://github.com/maksimr/vim-jsbeautify ~/.vim/bundle/ &
+git clone https://github.com/tpope/vim-markdown ~/.vim/bundle/ &
+git clone https://github.com/tpope/vim-sensible ~/.vim/bundle/ &
+git clone https://github.com/garbas/vim-snipmate ~/.vim/bundle/ &
+git clone https://github.com/honza/vim-snippets ~/.vim/bundle/ &
+git clone https://github.com/bronson/vim-trailing-whitespace ~/.vim/bundle/ &
+git clone https://github.com/mxw/vim-jsx ~/.vim/bundle/
